95ea2e86afcca11915de1d4a67a42d01e68be091
amelnichenko <maivashko@gmail.com>
2018-04-02 15:28:30 +0300

    g

--- am_01/src/Main.java
+++ am_01/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Hello sWorld!");
         System.out.println("Hello sWorld!");
         System.out.println("Hello Wsorld!");
+        System.out.println("Hssssssssssssello Wsorld!");
     }
     //fdghj
     //asdfghjk
